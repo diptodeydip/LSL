fc9ce20355f8a7be2358baffd35ada53795fe26c
diptodeydip <dipdey093@gmail.com>
2019-04-20 00:45:15 +0600

    New feactures - User Searching , Video Tutorial , Leaderboard

--- app/src/main/java/com/example/dip/lsl/Categories.java
+++ app/src/main/java/com/example/dip/lsl/Categories.java
@@ -37,7 +37,7 @@ public class Categories extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 MainActivity.category = "Days";
-               // finish();
+               //// finish();
                 startActivity(new Intent(Categories.this,Contents.class));
             }
         });
